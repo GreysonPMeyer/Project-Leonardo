c640c57555987f41117fc730d3e10aed0dd9d3b2
Greyson Meyer <123700168+GreysonPMeyer@users.noreply.github.com>
2024-11-20 18:51:35 -0800

    Tried to incorporate kmeans_final into the code
    I tried to put the kmeans code into the streamlit program as painlessly as I could. I don't know what's happening with requirements.txt. I only added the github portion, but there is all of a sudden a bunch of other stuff that appeared there. I really hope that doesn't mess anyone up

--- streamlitPlay.py
+++ streamlitPlay.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(
-    page_title="Project Leanardo", 
+    page_title="Project Leonardo", 
     page_icon="🖼️", 
     initial_sidebar_state="collapsed"
 )
@@ -11,6 +11,16 @@ import numpy as np
 import requests
 from PIL import Image
 from io import BytesIO
+import pandas as pd
+import requests
+import os
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import h5py
+from scipy.spatial.distance import cdist
+from scipy.optimize import linear_sum_assignment
+from k_means_stuff import k_means_final as kmeans
 
 
 ## page set up
@@ -39,8 +49,11 @@ if button_load_image: # if button clicked
         # Convert the file to an opencv image.
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
+        final_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
         # resize to 200 x 200
-        resized_image = cv2.resize(opencv_image, (200, 200), interpolation = cv2.INTER_LINEAR)
+        # resized_image = cv2.resize(opencv_image, (200, 200), interpolation = cv2.INTER_LINEAR)
+        resized_image = kmeans.resize_and_convert_image(final_image, (200, 200))
+        image_array = np.array(resized_image)
         # display in the center
         col1, col2, col3 = container_image_loader.columns(3)
         with col1:
@@ -57,7 +70,7 @@ if button_load_image: # if button clicked
             response = requests.get(text_URL, headers=headers)
             response.raise_for_status()
             image = Image.open(BytesIO(response.content))
-            resized_image = image.resize((200, 200))
+            resized_image = kmeans.resize_and_convert_image(image, (200, 200))
             # Convert image to numpy array
             image_array = np.array(resized_image)
             # display in the center
@@ -101,3 +114,16 @@ art_type = col3.selectbox(
 
 # button for generation
 form.form_submit_button(label="Submit")
+
+dataset_list = []
+
+img_color, color_title, img_comp, comp_title, img_overall, overall_title = kmeans.display_art(image_array, CC_ratio, dataset_list)
+
+images = [img_color, img_comp, img_overall]
+
+color_image = Image.open(img_color)
+st.image(color_image, caption=f"Color Recommendation: {color_title}", use_column_width=True)
+comp_image = Image.open(img_comp)
+st.image(comp_image, caption=f"Composition Recommendation: {comp_title}", use_column_width=True)
+overall_image = Image.open(img_overall)
+st.image(overall_image, caption=f"Overall Weighted Recommendation: {overall_title}", use_column_width=True)
