274f4d26fbbcab0e8ea15b8f8570cf5154ae77eb
Greyson Meyer <123700168+GreysonPMeyer@users.noreply.github.com>
2024-11-22 16:39:13 -0800

    StreamlitPlay is still flawed, but no longer broken
    StreamlitPlay can now be run through the terminal. I don't think the slider is updating based on user inputs and I don't think the submit button is working, but at least it isn't completely broken

--- streamlitPlay.py
+++ streamlitPlay.py
@@ -20,7 +20,7 @@ import matplotlib.image as mpimg
 import h5py
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
-from k_means_stuff import k_means_final as kmeans
+import k_means_stuff.k_means_final as kmeans
 
 
 ## page set up
@@ -43,6 +43,9 @@ uploaded_file = container_image_loader.file_uploader("Select an image...", type=
 text_URL = container_image_loader.text_input("or input image URL", value = "https://static.streamlit.io/examples/owl.jpg") # URL input
 button_load_image = container_image_loader.button(label = "Load Image") # button to load
 
+if "image_array" not in st.session_state:
+    st.session_state.image_array = None
+
 if button_load_image: # if button clicked
     # image loading from file
     if uploaded_file is not None:
@@ -53,13 +56,16 @@ if button_load_image: # if button clicked
         # resize to 200 x 200
         # resized_image = cv2.resize(opencv_image, (200, 200), interpolation = cv2.INTER_LINEAR)
         resized_image = kmeans.resize_and_convert_image(final_image, (200, 200))
-        image_array = np.array(resized_image)
+        st.session_state.image_array = np.array(resized_image)
+        print('this got reached!')
         # display in the center
+
+        
         col1, col2, col3 = container_image_loader.columns(3)
         with col1:
             st.write(' ')
         with col2:
-            st.image(resized_image, channels="BGR")
+            st.image(st.session_state.image_array)
         with col3:
             st.write(' ')
     else:
@@ -72,13 +78,12 @@ if button_load_image: # if button clicked
             image = Image.open(BytesIO(response.content))
             resized_image = kmeans.resize_and_convert_image(image, (200, 200))
             # Convert image to numpy array
-            image_array = np.array(resized_image)
-            # display in the center
+            st.session_state.image_array = np.array(resized_image)
             col1, col2, col3 = container_image_loader.columns(3)
             with col1:
                 st.write(' ')
             with col2:
-                st.image(image_array, channels="GRB")
+                st.image(st.session_state.image_array)
             with col3:
                 st.write(' ')
         except Exception as e:
@@ -98,12 +103,12 @@ k_cluster = col1.text_input(
     key="k-clusters",
 )
 # color / composition ratio
-CC_ratio = col2.slider(
+slider = col2.slider(
     "Color/Composition",
     min_value = 0.,
     max_value = 1.,
     value = 0.5, step=0.01,
-    key="CC-ratio",
+    key="slider",
 )
 # type of art
 art_type = col3.selectbox(
@@ -112,18 +117,20 @@ art_type = col3.selectbox(
     key = "style",
 )
 
+if "slider" not in st.session_state:
+    st.session_state.slider = slider
+
 # button for generation
 form.form_submit_button(label="Submit")
 
-dataset_list = []
-
-img_color, color_title, img_comp, comp_title, img_overall, overall_title = kmeans.display_art(image_array, CC_ratio, dataset_list)
-
+dataset_list = ["/Users/greysonmeyer/Downloads/resized_images_chunk_modfied_105.h5"]
+img_color, color_title, img_comp, comp_title, img_overall, overall_title = kmeans.display_art(st.session_state.image_array, st.session_state.slider, dataset_list)
 images = [img_color, img_comp, img_overall]
+# color_image = Image.open(img_color)
+# comp_image = Image.open(img_comp)
+# overall_image = Image.open(img_overall)
 
-color_image = Image.open(img_color)
-st.image(color_image, caption=f"Color Recommendation: {color_title}", use_column_width=True)
-comp_image = Image.open(img_comp)
-st.image(comp_image, caption=f"Composition Recommendation: {comp_title}", use_column_width=True)
-overall_image = Image.open(img_overall)
-st.image(overall_image, caption=f"Overall Weighted Recommendation: {overall_title}", use_column_width=True)
+col1, col2, col3 = form.columns([1, 3, 1])
+st.image(img_color, caption=f"Color Recommendation: {color_title}", use_container_width=True)
+st.image(img_comp, caption=f"Color Recommendation: {color_title}", use_container_width=True)
+st.image(img_overall, caption=f"Overall Weighted Recommendation: {overall_title}", use_container_width=True)
