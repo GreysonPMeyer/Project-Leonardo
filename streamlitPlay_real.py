ecb27fd64c61ea92a57af43b7376e106964d8b8b
2lineok <107795869+2lineok@users.noreply.github.com>
2024-11-29 21:39:45 -0500

    Update streamlitPlay_real.py
    remove resize(200,200)

--- streamlitPlay_real.py
+++ streamlitPlay_real.py
@@ -121,8 +121,8 @@ if button_load_image: # if button clicked
         pil_image = Image.open(BytesIO(uploaded_file.read()))
         # image_stream = BytesIO(uploaded_file.read())
         # pil_image = Image.open(image_stream)
-        resized_image = pil_image.resize((200, 200))
-        image_array = resize_and_convert_image(np.array(resized_image), (200, 200))
+        # resized_image = pil_image.resize((200, 200))
+        image_array = resize_and_convert_image(np.array(pil_image), (200, 200))
         st.session_state.image_array = image_array
 
         # file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
@@ -151,8 +151,8 @@ if button_load_image: # if button clicked
             response = requests.get(text_URL, headers=headers)
             response.raise_for_status()
             image = Image.open(BytesIO(response.content))
-            resized_image = image.resize((200,200))
-            image_array = resize_and_convert_image(np.array(resized_image), (200, 200))
+            # resized_image = image.resize((200,200))
+            image_array = resize_and_convert_image(np.array(image), (200, 200))
             st.session_state.image_array=image_array
 
             # response = requests.get(text_URL, headers=headers)
